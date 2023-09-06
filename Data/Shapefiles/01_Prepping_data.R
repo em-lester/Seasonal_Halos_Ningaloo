653691c887527e05e8187a7fbd81ec40994f651a
em-lester <emilykatelester@gmail.com>
2023-09-05 14:46:06 -1000

    loop to read in date from shapefile name and add to column, work out line length of each measurement, work out mean length per halo.
    2021 is done, repeat for for 2017-2021 and do giant loop with all data.

--- Data/Shapefiles/01_Prepping_data.R
+++ Data/Shapefiles/01_Prepping_data.R
@@ -38,7 +38,58 @@ shapefile.dir <- paste(data.dir,"Shapefiles",sep="/")
 
 # Read in the data----
 
+
 setwd(shapefile.dir)
 dir()
 
+# Define a vector with the filenames of the shapefiles
+shapefile_filenames <- c("Measurements_20210128_Cloates.shp", "Measurements_20210321_Cloates.shp",
+                         "Measurements_20210421_Cloates.shp", "Measurements_20210521_Cloates.shp",
+                         "Measurements_20210628_Cloates.shp", "Measurements_20210723_Cloates.shp",
+                         "Measurements_20210819_Cloates.shp", "Measurements_20210927_Cloates.shp",
+                         "Measurements_20211030_Cloates.shp", "Measurements_20211220_Cloates.shp")
+
+# Create an empty data frame to store mean lengths from all shapefiles
+all_mean_lengths <- data.frame()
+
+# Loop through the shapefile filenames
+for (filename in shapefile_filenames) {
+  cat("Processing:", filename, "\n")
+  
+  # Read the shapefile
+  shapefile <- st_read(filename)
+  
+  # Extract the date from the filename using regular expressions
+  date <- gsub(".*_(\\d{8})_.*", "\\1", filename)
+  
+  # Convert the shapefile to a dataframe
+  shapefile_df <- as.data.frame(shapefile) %>%
+    dplyr::select(id, Location, Status, Measure, length) %>%
+    glimpse()
+  
+  # Calculate the length of each line
+  shapefile_df$LineLength <- st_length(shapefile)
+  
+  # Add a "date" column to the dataframe
+  shapefile_df$date <- date
+  
+  # Group by 'date', 'id', and calculate the mean length for each group
+  mean_lengths <- shapefile_df %>%
+    na.omit() %>%
+    filter(Location == "Cloates") %>%
+    group_by(date, id) %>%
+    dplyr::mutate(MeanLength = mean(LineLength))
+  
+  # Append the mean lengths to the 'all_mean_lengths' data frame
+  all_mean_lengths <- bind_rows(all_mean_lengths, mean_lengths)
+  
+  cat("Processed:", filename, "\n")
+}
+
+glimpse(all_mean_lengths)
+
+setwd(tidy.dir)
+
+# Save all mean lengths as a single CSV file
+write.csv(all_mean_lengths, file = "2021_mean_lengths.csv", row.names = FALSE)
 
